c7388dcea3eead38ee98b4e8021b96a2d89adf3c
Mynta Developer <dev@myntacoin.org>
2026-01-16 21:40:03 -0800

    build: Fix macOS native build with FORCE_USE_SYSTEM_CLANG
    When building natively on macOS with FORCE_USE_SYSTEM_CLANG=1, the
    depends system was failing to find C++ headers because:
    
    1. OSX_SDK pointed to an extracted SDK path that doesn't exist
    2. The compiler flags used complex -Xclang options designed for
       cross-compilation with depends-managed clang
    
    This commit restructures darwin.mk to properly handle native builds:
    - Use xcrun --show-sdk-path to detect the native Xcode SDK
    - Use simpler -isysroot flag instead of -Xclang include path hacks
    - Use system tools (ar, ranlib, etc.) instead of cctools
    - Skip native_cctools and native_binutils dependencies
    
    The cross-compilation path remains unchanged for deterministic builds.
    
    Fixes Boost build failure: 'cstddef' file not found

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -4,13 +4,13 @@ XCODE_VERSION=11.3.1
 XCODE_BUILD_ID=11C505
 LD64_VERSION=530
 
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+# FORCE_USE_SYSTEM_CLANG is empty, so we use our depends-managed, pinned clang
+# from llvm.org with the extracted SDK
+
 OSX_SDK=$(SDK_PATH)/Xcode-$(XCODE_VERSION)-$(XCODE_BUILD_ID)-extracted-SDK-with-libcxx-headers
 
 darwin_native_binutils=native_cctools
-
-ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-# FORCE_USE_SYSTEM_CLANG is empty, so we use our depends-managed, pinned clang
-# from llvm.org
 
 # Clang is a dependency of native_cctools when FORCE_USE_SYSTEM_CLANG is empty
 darwin_native_toolchain=native_cctools
@@ -21,8 +21,12 @@ clangxx_prog=$(clang_prog)++
 clang_resource_dir=$(build_prefix)/lib/clang/$(native_clang_version)
 else
 # FORCE_USE_SYSTEM_CLANG is non-empty, so we use the clang from the user's
-# system
+# system with the native Xcode SDK
 
+# Use xcrun to find the SDK - this works on both Intel and Apple Silicon
+OSX_SDK:=$(shell xcrun --show-sdk-path 2>/dev/null)
+
+darwin_native_binutils=
 darwin_native_toolchain=
 
 # We can't just use $(shell command -v clang) because GNU Make handles builtins
@@ -43,10 +47,21 @@ cctools_TOOLS=AR RANLIB STRIP NM LIBTOOL OTOOL INSTALL_NAME_TOOL
 # Make-only lowercase function
 lc = $(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$1))))))))))))))))))))))))))
 
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
 # For well-known tools provided by cctools, make sure that their well-known
 # variable is set to the full path of the tool, just like how AC_PATH_{TOO,PROG}
 # would.
 $(foreach TOOL,$(cctools_TOOLS),$(eval darwin_$(TOOL) = $$(build_prefix)/bin/$$(host)-$(call lc,$(TOOL))))
+else
+# Use system tools when building natively with system clang
+darwin_AR=$(shell $(SHELL) $(.SHELLFLAGS) "command -v ar")
+darwin_RANLIB=$(shell $(SHELL) $(.SHELLFLAGS) "command -v ranlib")
+darwin_STRIP=$(shell $(SHELL) $(.SHELLFLAGS) "command -v strip")
+darwin_NM=$(shell $(SHELL) $(.SHELLFLAGS) "command -v nm")
+darwin_LIBTOOL=$(shell $(SHELL) $(.SHELLFLAGS) "command -v libtool")
+darwin_OTOOL=$(shell $(SHELL) $(.SHELLFLAGS) "command -v otool")
+darwin_INSTALL_NAME_TOOL=$(shell $(SHELL) $(.SHELLFLAGS) "command -v install_name_tool")
+endif
 
 # Flag explanations:
 #
@@ -95,6 +110,8 @@ $(foreach TOOL,$(cctools_TOOLS),$(eval darwin_$(TOOL) = $$(build_prefix)/bin/$$(
 #         include search paths, as that would be wrong in general but would also
 #         break #include_next's.
 #
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+# Cross-compilation with depends-managed clang - need explicit include paths
 darwin_CC=env -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
               -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
               -u LIBRARY_PATH \
@@ -113,6 +130,13 @@ darwin_CXX=env -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
                -Xclang -cxx-isystem$(OSX_SDK)/usr/include/c++/v1 \
                -Xclang -internal-externc-isystem$(clang_resource_dir)/include \
                -Xclang -internal-externc-isystem$(OSX_SDK)/usr/include
+else
+# Native compilation with system clang - use -isysroot, clang handles headers
+darwin_CC=$(clang_prog) -mmacosx-version-min=$(OSX_MIN_VERSION) \
+            -isysroot$(OSX_SDK)
+darwin_CXX=$(clangxx_prog) -mmacosx-version-min=$(OSX_MIN_VERSION) \
+             -isysroot$(OSX_SDK) -stdlib=libc++
+endif
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
