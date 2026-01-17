3022d4350f45af9ed9fa210df3ae42c6d6805c56
Mynta Developer <dev@myntacoin.org>
2026-01-17 05:55:27 -0800

    build: Fix miniupnpc sed pattern for .d dependency removal
    Use a line-matching sed pattern that doesn't rely on make variable
    escaping. The pattern `/%.o:.*%.c/s/ [^ ]*%.d//` finds lines with
    the object file rule and removes the .d file dependency.
    
    Previous patterns failed due to complex escaping of $(BUILD) in
    the make -> shell -> sed chain.
    
    Tested locally: confirmed the pattern correctly transforms:
      $(BUILD)/%.o: $(SRCDIR)/%.c $(BUILD)/%.d
    to:
      $(BUILD)/%.o: $(SRCDIR)/%.c
    
    Fixes: No rule to make target 'build/miniwget.d'

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -15,7 +15,7 @@ define $(package)_preprocess_cmds
   mkdir -p dll build && \
   sed -e 's|MINIUPNPC_VERSION_STRING \"version\"|MINIUPNPC_VERSION_STRING \"$($(package)_version)\"|' -e 's|OS/version|$(host)|' miniupnpcstrings.h.in > miniupnpcstrings.h && \
   sed -i.old "s|miniupnpcstrings.h: miniupnpcstrings.h.in wingenminiupnpcstrings|miniupnpcstrings.h: miniupnpcstrings.h.in|" Makefile.mingw && \
-  sed -i.old 's| $$(BUILD)/%.d||g' Makefile
+  sed -i.old '/%.o:.*%.c/s/ [^ ]*%.d//' Makefile
 endef
 
 define $(package)_build_cmds
